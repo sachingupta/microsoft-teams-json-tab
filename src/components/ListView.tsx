a6b4551b81757796397c0661b09acf3689f52cd0
Adrian Tirado-Medina <t-adtira@microsoft.com>
2019-05-20 14:32:46 -0700

    Created ListView Component Render Process

--- src/components/ListView.tsx
+++ src/components/ListView.tsx
@@ -1,10 +1,25 @@
 import React from 'react'
+import { List, Image } from '@stardust-ui/react'
 
-export const ListView: React.FC = () => {
-    
+export const ListView: React.FC = (props) => {
+    let out_list:Array<any>;
+    let key_count = 0
+    const process_item = (item:any) => {
+        key_count++
+        let out = {
+            key: key_count,
+            header: item.title,
+            content: item.subTitle,
+            media: <Image src={item.heroImageSrc} avatar/>
+        }
+        return out;
+    }
+
+    out_list = (props.itemList).map(process_item);
+
     return (
         <div>
-            ListView
+            <List selectable defaultSelectedIndex={0} items = {out_list}/>
         </div>    
     )
 }
