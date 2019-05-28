fe706905241ecf2c187cbe523aa3fd62743c3678
Adrian Tirado-Medina <t-adtira@microsoft.com>
2019-05-28 14:44:37 -0700

    Moved Result Interface to the Results component

--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import { ListView } from './ListView'
-import { IState } from '../jsonTabs.interface';
 import { CardView } from './CardView';
+import { IPreviewCard } from '../api/api.interface';
 
+export interface IResultState {
+  results : Array<IPreviewCard>;
+  viewOption: string;  
+}
 enum viewOption{
   List = 'List',
   Grid = 'Grid'
 }
-export const Results = (props: IState) =>{
+export const Results = (props: IResultState) =>{
   return (
     <div>
       {props.viewOption === viewOption.List ? <ListView itemList={ props.results } /> : <CardView itemList={ props.results } />}
