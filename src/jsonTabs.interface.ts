dcf097907269c535201d023afd7adefa69de6dea
Adrian Tirado-Medina <t-adtira@microsoft.com>
2019-05-28 11:57:44 -0700

    Added New Interfaces for processing

--- src/jsonTabs.interface.ts
+++ src/jsonTabs.interface.ts
@@ -9,3 +9,17 @@ export interface IState {
     results : Array<IPreviewCard>;
     viewOption: string;  
 }
+
+export interface ITaskModule {
+    title: string;
+    height: number;
+    width: number;
+    url: string;
+    card: any;
+}
+
+export interface ICardResponse {
+    contentType: string;
+    content: any;
+    preview: IPreviewCard;
+}
