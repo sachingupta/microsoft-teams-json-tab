09f2e18fe6a8df6ea57f25bf2ee42036e87f2549
Adrian Tirado-Medina <t-adtira@microsoft.com>
2019-05-28 14:44:07 -0700

    added ulities file for task module actions

---
+++ src/utils/utils.ts
@@ -0,0 +1,13 @@
+import * as microsoftTeams from '@microsoft/teams-js';
+import { ICard } from "../api/api.interface";
+
+export const displayTaskModule = (response: ICard) => {
+    const taskInfo: microsoftTeams.TaskInfo = {
+        height: undefined,
+        width: undefined,
+        title: response.preview.heroImageSrc,
+        url: undefined,
+        card: response.content
+    }
+    microsoftTeams.tasks.startTask(taskInfo);
+}
