f1624681abda2f229ad5e4eedbb8a451f3f9a770
FaizChishtie <faizchishtie@gmail.com>
2019-06-05 09:31:35 -0700

    fix merge conf

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,24 +1,15 @@
-<<<<<<< HEAD
-import { default as jsonData } from '../generated.json';
-import { IPreviewCard } from '../jsonTabs.interface.js';
-import * as microsoftTeams from '@microsoft/teams-js'
-=======
 import { default as jsonData } from './generated.json';
 import { ICard } from '../api/api.interface';
->>>>>>> 20c86531c2bc41154e719769da2e50a8110e14b3
+import * as microsoftTeams from '@microsoft/teams-js';
 
 export const getResults = ( query: string ): ICard[] => {
     if( !query ) {
         return jsonData;
     }
 
-<<<<<<< HEAD
-    microsoftTeams.getBotData({ query } , (data: any) => { console.log(data) });
+    microsoftTeams.getBotData( { query } , ( data: any ) => { console.log( data ) } );
 
-    var queriedItems: Array<IPreviewCard> = [];
-=======
     const queriedItems: ICard[] = [];
->>>>>>> 20c86531c2bc41154e719769da2e50a8110e14b3
 
     jsonData.forEach( ( item: ICard ) => {
         if( item && item.preview.title.toLowerCase().includes( query.trim().toLowerCase() ) ){
