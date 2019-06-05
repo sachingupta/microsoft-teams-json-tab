c009bb44a241ba9ee8fe4a3d2f6dd65dc1ba3a3f
FaizChishtie <faizchishtie@gmail.com>
2019-06-05 09:27:34 -0700

    committing old changes

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,10 +1,13 @@
 import { default as jsonData } from '../generated.json';
 import { IPreviewCard } from '../jsonTabs.interface.js';
+import * as microsoftTeams from '@microsoft/teams-js'
 
 export const getResults = (query: string): Array<IPreviewCard> => {
     if(!query) {
         return jsonData;
     }
+
+    microsoftTeams.getBotData({ query } , (data: any) => { console.log(data) });
 
     var queriedItems: Array<IPreviewCard> = [];
 
