268a87a4cd1cc78ade17e221dc48a5f6d52ef94f
Sachin Gupta <saching@microsoft.com>
2019-06-18 14:04:52 -0700

    adding build folder

---
+++ build/precache-manifest.3057b307e5825a3a6717673e54f0fd2f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cea922176b1801e1aa9f0444c9641ec4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "89c289e5596a10607492",
+    "url": "/static/css/main.eafdf11b.chunk.css"
+  },
+  {
+    "revision": "56dbd08221eba4011332",
+    "url": "/static/js/2.ab9a14c4.chunk.js"
+  },
+  {
+    "revision": "89c289e5596a10607492",
+    "url": "/static/js/main.963d3383.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
