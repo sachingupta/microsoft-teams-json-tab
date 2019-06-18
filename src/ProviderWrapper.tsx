8b0e7315658d84f1b8e680623e4b016d20222681
Adrian Tirado-Medina <t-adtira@microsoft.com>
2019-06-18 11:24:34 -0700

    Fixed ProviderWrapper

--- src/ProviderWrapper.tsx
+++ src/ProviderWrapper.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import { Provider, ThemeInput } from '@stardust-ui/react';
+import { Provider } from '@stardust-ui/react';
 import App from './App';
 import * as themeUtils from './utils/themeUtils';
 
-export const ProviderWrapper = (props: {}) => {
+export const ProviderWrapper: React.FC = (): JSX.Element => {
   // CONSTANT
   const initialTheme = themeUtils.getTheme(themeUtils.getThemeFromURL(window.location.href));
   // HOOKS
   const [Theme, setTheme] = React.useState(initialTheme);
 
   // HANDLERS
-  const handleThemeChange = (theme: string) => {
+  const handleThemeChange = (theme: string): void => {
     setTheme(themeUtils.getTheme(theme));
   };
 
