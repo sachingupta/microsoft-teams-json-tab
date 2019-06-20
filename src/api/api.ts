93d03dc9b402dfde164b489fa5171c19909fad45
Adrian Tirado-Medina <t-adtira@microsoft.com>
2019-06-20 15:17:34 -0700

    add disableUnsupportedActions API

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,10 +1,11 @@
 import * as microsoftTeams from '@microsoft/teams-js';
+import * as adaptiveCards from 'adaptivecards';
 
 export const getResults = (
-  query: string,
-  // should be microsoftTeams.bot.QueryResponse
+  query: string, 
   onResults: (response: microsoftTeams.bot.QueryResponse) => void,
   onError: (error: string) => void,
+  commandId?: string
 ): void => {
   microsoftTeams.bot.sendQuery({ query }, onResults, onError);
 };
@@ -15,3 +16,13 @@ export const getSupportedCommands = (
 ): void => {
   microsoftTeams.bot.getSupportedCommands(onBotGetCommandResponse, onError);
 };
+
+// export const disableUnsupportedActions = (card: adaptiveCards.IAdaptiveCard) => {
+//   const SupportedActions: string[] = ['Action.OpenUrl', 'Action.Submit', 'Action.ShowCard', 'invoke', 'signin'];
+
+//   if(card.actions) {
+//     card.actions.forEach((item:any) => {
+  
+//     })
+//   }
+// }
