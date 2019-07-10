3f322bbf5d6cdc8e067dd433e327d6e2a784410c
Sachin Gupta <saching@microsoft.com>
2019-07-09 17:06:41 -0700

    updating Build folder

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f79fa88b9c7d201fc44865b309990907.js"
+  "/precache-manifest.db38368ffdec166cb538008a569a2df4.js"
 );
 
 self.addEventListener('message', (event) => {
