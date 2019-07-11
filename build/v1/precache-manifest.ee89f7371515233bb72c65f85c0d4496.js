1e3cf349dc53d953fcde058b693fb795f867cd6a
Sachin Gupta <saching@microsoft.com>
2019-07-11 11:08:09 -0700

    adding build v1 folder

---
+++ build/v1/precache-manifest.ee89f7371515233bb72c65f85c0d4496.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df04d4a0bbf78efb1add3e06a9798573",
+    "url": "/index.html"
+  },
+  {
+    "revision": "944530e444e3e14ea1b9",
+    "url": "/static/css/main.c8c6283a.chunk.css"
+  },
+  {
+    "revision": "7c3645362477dc1f7c91",
+    "url": "/static/js/2.0560bd67.chunk.js"
+  },
+  {
+    "revision": "944530e444e3e14ea1b9",
+    "url": "/static/js/main.9e008428.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
